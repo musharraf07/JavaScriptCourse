df6a40d2f90e86f1b1aef2a744ce4ff64fda389d
Musharraf <121166414+musharraf07@users.noreply.github.com>
2023-06-22 17:01:02 +0000

    Arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,52 @@
+const myArr = [0,1,2,3,4,5]
+// console.log(myArr[0]);
+const myArr2 = ["musharraf", "yusuf"]
+
+
+const myArr1 = new Array(1,2,3,4,5)
+
+
+// Array methods
+
+// myArr.push(8)
+// console.log(myArr);
+
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr)
+
+// myArr.unshift(0)
+// console.log(myArr)
+
+// console.log(myArr.includes(1))
+// console.log(myArr.indexOf(2))
+
+// const newArr = myArr.join()
+// console.log(newArr)
+// console.log(typeof newArr)
+
+// concat 
+// const arr = [0,1,2,3,4,5]
+// const arr2 = ["musharraf", "yusuf"]
+
+// const arr3 = myArr1.concat(arr2)
+// console.log(arr3);
+// console.log(arr)
+
+
+
+// Slics , Splice
+
+// console.log("A", myArr)
+
+// const myn1 = myArr.slice(1,3)
+// console.log(myn1)
+
+// console.log("B", myArr)
+
+// splice
+// const myn2 = myArr.splice(1,3)
+// console.log(myn2)
+// console.log("C", myArr)
---
+++ 02_Basics/02_arrays.js
@@ -0,0 +1,30 @@
+const marvel = ["Thor", "ironman", "spiderman"]
+const dc = ["superman", 'flash',"batman"]
+
+// marvel.push(dc)
+// console.log(marvel)
+
+// concat
+// const marv = marvel.concat(dc)
+// console.log(marv)
+
+// spread operator 
+
+// const all_new_heors = [...marvel, ...dc]
+// console.log(all_new_heors)
+
+// flat()
+
+// const arr1 = [1,2,3,4,[4,5,6,6],3,[3,4,3,[8,9]]]
+// const realArr = arr1.flat(Infinity)
+// console.log(realArr)
+
+// console.log(Array.isArray("musharraf"));
+// console.log(Array.from("musharraf"));
+// console.log(Array.from({name: "Musharraf"}));
+
+
+// let score1 = 100
+// let score2 = 202
+// let score3 = 300
+// console.log(Array.of(score1,score2,score3))
