209023365f550fca05b79c93ef6f4838fa71b6bc
Mohammed Musharraf <121166414+musharraf07@users.noreply.github.com>
2023-07-13 16:06:04 +0000

    project2

---
+++ 07_projects/two/chaiaurcode.js
@@ -0,0 +1,41 @@
+const form = document.querySelector('form');
+
+
+// // const weight = parseInt(document.querySelector('#weight'.value))     
+// // this will give empty value 
+
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
+
+
+    if (height === ' ' || height < 0 || isNaN(height)){
+        results.innerHTML = `please give a valid height ${height}`;
+    }
+    else if (weight ==='' || weight < 0 || isNaN(weight)){
+        results.innerHTML = `please give a valid height ${weight}`;
+    }
+    else {
+        const bmi = (weight/((height*height)/10000)).toFixed(2);
+
+        if(bmi< 18.6){
+            results.innerHTML = `<span>${bmi}</span> <br> <span> Under Weight  </span>`;
+        }
+        else if (bmi < 24.9 && bmi>18.6){
+            results.innerHTML = `<span>${bmi}</span> <br> <span> Normal range </span>`;
+        }
+        else{
+            results.innerHTML = `<span>${bmi}</span> <br> <span> Over Weight </span>`;
+        }
+        
+    }
+});
+
+
+
+
