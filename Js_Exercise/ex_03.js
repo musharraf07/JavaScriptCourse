35ed953e770ea4d46a4a65e65556701cd9a33812
Mohammed Musharraf <121166414+musharraf07@users.noreply.github.com>
2023-07-16 18:17:41 +0000

    exercise

---
+++ Js_Exercise/ex_03.js
@@ -0,0 +1,77 @@
+// Q1
+// const button = document.getElementById('button');
+
+// button.addEventListener('click', function () {
+//     button.innerHTML = 'submitted';
+    
+// })
+
+
+// Q2
+
+// function addClassOnMouseOver(elementID, className) {
+//     const element = document.getElementById(elementID);
+//     element.addEventListener("mouseover", () => {
+//       element.classList.add(className);
+//     });
+//   } 
+ 
+//   addClassOnMouseOver("button", "active");
+
+
+
+// Q3
+
+// function removeClassOnScroll(elementID, className){
+//     const element = document.getElementById(elementID);
+//     element.addEventListener("scroll", () => {
+//         element.classList.remove(className);
+//         console.log("class removed");
+//     })
+// }
+// removeClassOnScroll("button", "active")
+
+
+// Q4
+// function toggleDivDisplayOnClick(divId) {
+//     const div = document.getElementById(divId);
+//     if (div.style.display === "none") {
+//       div.style.display = "block";
+//     } else {
+//       div.style.display = "none";
+//     }
+//   }
+  
+
+// Q5
+
+
+ function validateForm (event){
+    event.preventDefault();
+
+    let isValid =true;
+
+    for(let i = 0; i<form.elemenst.length; i++){
+        const input = form.element[i]
+        const value = input.value.trim()
+
+        if (input.hasAttribute("required" && value === "")) {
+            isValid = false;
+            input.classList.add('invalid');
+        }
+        else{
+            input.classList.add('invalid');
+        }
+    }
+
+    const message = document.getElementById("message");
+  if (isValid) {
+    message.classList.remove("error");
+    message.innerText = "Form submitted successfully!";
+  } else {
+    message.classList.add("error");
+    message.innerText = "Please fill out all required fields.";
+  }
+}
+const form = document.getElementById("form");
+form.addEventListener("submit", validateForm); 
