85793278068740f84a8b9c0a35bdd1fdb3e2825d
Mohammed Musharraf <121166414+musharraf07@users.noreply.github.com>
2023-08-03 13:13:27 +0000

    practice set 1

---
+++ Practice_Set/01_practice.js
@@ -0,0 +1,39 @@
+// Q1 
+
+// let a = "hello";
+// let b = 3;
+
+// console.log(a+b);
+
+// Q2 
+// console.log(typeof (a+b));
+
+// Q3 
+
+const obj = {
+    name: "mush",
+    section:1,
+    age:19
+}
+// obj = 23;
+
+
+// Q4
+obj['friend'] = 'yusuf'
+obj['name'] = 'Musharraf'
+console.log(obj);
+
+
+// Q5
+
+const dict = {
+    appreciate:"recognize the full worth of.",
+    bipolar:"having or relating to two poles or extremities.",
+    colleague:"a person with whom one works in a profession or business."
+
+}
+
+console.log(dict.bipolar);
+console.log(dict['colleague']);
+
+
