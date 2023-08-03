02ebb1af605eec49a22c71aab68812838b87f7bb
Mohammed Musharraf <121166414+musharraf07@users.noreply.github.com>
2023-08-03 13:30:42 +0000

    practice set 2

---
+++ Practice_Set/02_practice.js
@@ -0,0 +1,73 @@
+// Q1 
+/*
+let age = 12;
+age = Number.parseInt(age);
+
+if(age>10 && age<20){
+    console.log("you are eligible to play");
+}
+else{
+    console.log("you are not eligible to play");
+}
+*/
+
+// Q2
+/*
+let age = 12;
+
+switch(age){
+    case 12:
+        console.log("your age is 12");
+        break;
+    case 15:
+        console.log("your age is 15");
+        break;
+    case 10:
+        console.log("your age is 10");
+        break;
+    case 11:
+        console.log("your age is 11");
+        break;
+    case 14:
+        console.log("your age is 14");
+        break;
+    default:
+        console.log("you are not special");
+    }
+
+*/
+
+
+// Q3
+/*
+let num = 21;
+
+if(num%2==0 && num%3==0){
+    console.log("your number is divisible by 2 and 3");
+}
+
+else{
+    console.log("your number is not divisible by 2 and 3");
+}
+
+*/
+
+// Q4
+/*
+let num = 21;
+
+if(num%2==0 || num%3==0){
+    console.log("your number is divisible by 2 or 3");
+}
+
+else{
+    console.log("your number is not divisible by 2 and 3");
+}
+
+*/
+
+
+// Q5 
+let age = 13;
+let a = age>18? "you can drive" : "you cannot drive";
+console.log(a);
