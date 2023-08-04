b1d1af3fe3b6ebbb094f7e1d578ea156299c9621
Mohammed Musharraf <121166414+musharraf07@users.noreply.github.com>
2023-08-04 14:52:51 +0000

    practice 4

---
+++ Practice_Set/04_practice.js
@@ -0,0 +1,30 @@
+// Q1
+let str = "mush\"";
+console.log(str.length);
+
+// Q2
+const sentence = "my name is Musharraf ";
+const word = "Mohammed";
+console.log(sentence.includes(word));
+
+
+// Q3
+const name = "MUSHARRAF";
+console.log(name.toLowerCase());
+
+// Q4
+let str1 = "my name is Musharraf Rahamathulla"
+
+let s = str1.slice(11)
+console.log(s);
+console.log(typeof(s));
+
+// Q5
+
+let friend = "yusuf";
+friend[2] ="M"
+console.log(friend); // friend is not changed because string is immutable 
+
+// Q6
+
+
